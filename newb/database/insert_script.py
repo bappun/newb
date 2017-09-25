faadd70423176e95d9a94bd9e109b5dc84109abd
Minecraftienne <tiny99@free.fr>
2017-09-25 14:15:50 +0200

    models + script insert

---
+++ newb/database/insert_script.py
@@ -0,0 +1,10 @@
+platforms = [
+    ['Windows', 'Microsoft', None],
+    ['Xbox One', 'Microsft', 2013],
+    ['PS4', 'Sony', 2013],
+    ['Switch', 'Nintendo', 2017]
+]
+
+for platform in platforms:
+    Platform(name=platforms[0], constructor=platforms[1], release_year=platforms[2]).save()
+

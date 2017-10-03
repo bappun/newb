aac0ea79388833726554dcdb02584ab1d6bedca9
Tiffany HAUMEY <tiffany.haumey@gmail.com>
2017-10-03 14:14:51 +0200

    update

--- newb/apps/shop/database/insert_script.py
+++ newb/apps/shop/database/insert_script.py
@@ -45,7 +45,8 @@ if __name__ == '__main__':
         ['Activision'],
         ['Nintendo'],
         ['Turn 10'],
-        ['Bluehole, Inc.']
+        ['Bluehole, Inc.'],
+        ['Microsoft']
     ]
 
     for developer in developers:
@@ -56,19 +57,8 @@ if __name__ == '__main__':
         ['Fifa 18', '2017-09-29', [Genre.objects.get(name='Sport')], [Platform.objects.get(name='Windows'), Platform.objects.get(name='Xbox One'), Platform.objects.get(name='PS4'), Platform.objects.get(name='Switch')], Editor.objects.get(name='EA Sports'), Developer.objects.get(name='EA Sports'), '/media/uploaded/img/shop/fifa_18.jpg', 'FIFA 18 est un jeu de simulation de football sur PC, PS4 et Xbox One édité par Electronic Arts. Le mode aventure promet d\'être retravaillé en profondeur, tout en faisant suite à celui du précédent opus. Le championnat chinois fera également son apparition pour la première fois, sans oublier les traditionnelles améliorations apportées aux graphismes et au gameplay, ainsi que le retour de Pierre Ménès et Hervé Mathoux aux commentaires.', 50.99],
         ['Destiny 2', '2017-09-06', [Genre.objects.get(name='Tir')], [Platform.objects.get(name='Windows'), Platform.objects.get(name='Xbox One'), Platform.objects.get(name='PS4')], Editor.objects.get(name='Activision'), Developer.objects.get(name='Bungie'), '/media/uploaded/img/shop/destiny_2.jpg', 'Suite du premier opus du même nom réalisé par les créateurs de Halo, Destiny 2 est un FPS futuriste et intergalactique. Les joueurs peuvent y importer les personnages qu\'ils ont créés et améliorés dans le premier opus.', 44.99],
         ['Super Mario Odyssey', '2017-10-27', [Genre.objects.get(name='Plate-forme')], [Platform.objects.get(name='Switch')], Editor.objects.get(name='Nintendo'), Developer.objects.get(name='Nintendo'), '/media/uploaded/img/shop/super_mario_odyssey.jpg', 'La princesse Peach a été enlevée par Bowser. Pour la sauver, Mario quitte le royaume Champignon à bord de l\'Odyssey. Accompagné de Clappy, son chapeau vivant, il parcourra différents royaumes et fera tout pour vaincre, une nouvelle fois, terrible Bowser.', 44.90],
-        ['Forza Motorsport 7', '2017-10-03', [Genre.objects.get(name='Course'), Genre.objects.get(name='Simulation')],
-         [Platform.objects.get(name='Xbox One'), Platform.objects.get(name='Windows')],
-         Editor.objects.get(name='Microsoft'), Developer.objects.get(name='Turn 10'),
-         '/media/uploaded/img/shop/forza_motorsport_7.jpg',
-         'Forza Motorsport 7 est un jeu de course automobile développé par Turn 10. Septième opus de la série, il bénéficie d\'un mode carrière revu en profondeur et de nouveautés comme le système de météo dynamique.',
-         54.99],
-        ['PlayerUnknown\'s Battlegrounds', '2017-03-23',
-         [Genre.objects.get(name='MMO'), Genre.objects.get(name='Action'), Genre.objects.get(name='Aventure'),
-          Genre.objects.get(name='Tir')], [Platform.objects.get(name='Windows')],
-         Editor.objects.get(name='Bluehole, Inc.'), Developer.objects.get(name='Bluehole, Inc.'),
-         '/media/uploaded/img/shop/playerunknowns_battlegrounds.png',
-         'PlayerUnknown\'s Battlegrounds est un jeu multijoueur de type Battle Royale. En partant de rien, il vous faut trouver des armes et des ressources afin d\'être le dernier survivant. Développé avec Unreal Engine 4, vous vous retrouvez sur une île de 8x8 km, avec pour but ultime de ne pas vous faire tuer.',
-         23.99]
+        ['Forza Motorsport 7', '2017-10-03', [Genre.objects.get(name='Course'), Genre.objects.get(name='Simulation')], [Platform.objects.get(name='Xbox One'), Platform.objects.get(name='Windows')], Editor.objects.get(name='Microsoft'), Developer.objects.get(name='Turn 10'), '/media/uploaded/img/shop/forza_motorsport_7.jpg', 'Forza Motorsport 7 est un jeu de course automobile développé par Turn 10. Septième opus de la série, il bénéficie d\'un mode carrière revu en profondeur et de nouveautés comme le système de météo dynamique.', 54.99],
+        ['PlayerUnknown\'s Battlegrounds', '2017-03-23', [Genre.objects.get(name='MMO'), Genre.objects.get(name='Action'), Genre.objects.get(name='Aventure'), Genre.objects.get(name='Tir')], [Platform.objects.get(name='Windows')], Editor.objects.get(name='Bluehole, Inc.'), Developer.objects.get(name='Bluehole, Inc.'), '/media/uploaded/img/shop/playerunknowns_battlegrounds.png', 'PlayerUnknown\'s Battlegrounds est un jeu multijoueur de type Battle Royale. En partant de rien, il vous faut trouver des armes et des ressources afin d\'être le dernier survivant. Développé avec Unreal Engine 4, vous vous retrouvez sur une île de 8x8 km, avec pour but ultime de ne pas vous faire tuer.', 23.99]
     ]
 
     i=1

75c1c74af174d398b4e8efba15de6315b8a58d0a
bappun <bapt.rouxel@gmail.com>
2017-10-03 14:16:25 +0200

    new views and style update

--- newb/apps/shop/urls.py
+++ newb/apps/shop/urls.py
@@ -5,4 +5,9 @@ from newb.apps.shop import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^products/$', views.products, name="products"),
+    url(r'^login/$', views.login, name="login"),
+    url(r'^logout/$', views.logout, name="logout"),
+    url(r'^register/$', views.register, name="register"),
+    url(r'^account/$', views.account, name="account"),
+    url(r'^contact/$', views.contact, name="contact"),
 ]
--- newb/apps/shop/views.py
+++ newb/apps/shop/views.py
@@ -13,3 +13,29 @@ def products(request):
         'products': VideoGame.objects.all()
     }
     return render(request, 'shop/products.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'shop/login.html', context)
+
+
+def logout(request):
+    context = {}
+    return render(request, 'shop/logout.html', context)
+
+
+def register(request):
+    context = {}
+    return render(request, 'shop/register.html', context)
+
+
+def account(request):
+    context = {}
+    return render(request, 'shop/account.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request, 'shop/contact.html', context)
+
